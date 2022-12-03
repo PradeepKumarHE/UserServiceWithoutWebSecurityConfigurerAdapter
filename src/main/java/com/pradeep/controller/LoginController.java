bed7f177e4ac524bc5f49b9b46f5158e615168e2
Pradeep <pradeepkumarhe1989@gmail.com>
2022-12-03 21:39:20 +0530

    config related completed

---
+++ src/main/java/com/pradeep/controller/LoginController.java
@@ -0,0 +1,4 @@
+package com.pradeep.controller;
+
+public class LoginController {
+}
---
+++ src/main/java/com/pradeep/entities/User.java
@@ -0,0 +1,19 @@
+package com.pradeep.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String encryptedEmail;
+    private Boolean isActive=true;
+    private Boolean isNotLocked=true;
+}
